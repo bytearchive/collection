d432ae39c70db1bfc7adcaaf8c2e55958bcafc57
Flynn Wang <freefeb@gmail.com>
2012-02-08 14:54:21 +0800

    change wsgi.py with change to apache folder

--- apache/wsgi.py
+++ apache/wsgi.py
@@ -7,7 +7,9 @@ import os
 os.environ["CELERY_LOADER"] = "django"
 
 # setup the environment for Django and Pinax
-pinax.env.setup_environ(__file__)
+file_path = os.path.dirname(__file__)
+project_path = os.path.dirname(file_path)
+pinax.env.setup_environ(project_path=project_path)
 
 # set application for WSGI processing
 application = WSGIHandler()
