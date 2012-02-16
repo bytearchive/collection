b037bd23d28d0ca5095e55a882b20331ccf18221
Flynn Wang <wang.flynn@gmail.com>
2012-02-16 16:19:56 +0800

    remove author and article publication date

--- reader/tasks.py
+++ reader/tasks.py
@@ -1,7 +1,6 @@
 from celery.decorators import task
 from reader.models import Article, UserProfile
 from reader.extractor import get_article, get_article_meta
-import traceback
 
 @task(ignore_result=True)
 def process_article_task(user_id, url, html):
